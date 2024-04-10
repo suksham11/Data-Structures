9c4bcdbf7048325f3c4b93400ce7f4dc1fec2944
suksham <129354626+suksham11@users.noreply.github.com>
2024-04-10 23:20:32 +0530

    Update Maximize dot product.cpp

--- GFG/Maximize dot product.cpp
+++ GFG/Maximize dot product.cpp
@@ -1,8 +1,4 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-// } Driver Code Ends
 class Solution{
 		
 	public:
@@ -31,33 +27,3 @@ class Solution{
 };
 
 
-//{ Driver Code Starts.
-int main() 
-{
-   
-   
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, m;
-        cin >> n >> m;
-
-        int a[n], b[m];
-
-        for(int i = 0; i < n; i++)
-        	cin >> a[i];
-
-        for(int i = 0; i < m; i++)
-        	cin >> b[i];
-
-       
-
-	    Solution ob;
-	    cout << ob.maxDotProduct(n, m, a, b) << "\n";
-	     
-    }
-    return 0;
-}
-
-// } Driver Code Ends
