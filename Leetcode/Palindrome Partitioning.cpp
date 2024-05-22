4e6a34e0e82795488d04ea83e9b01c5a655127d9
suksham <129354626+suksham11@users.noreply.github.com>
2024-05-22 20:45:38 +0530

    Create Palindrome Partitioning.cpp

---
+++ Leetcode/Palindrome Partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> path;
+        backtrack(s , 0 , path , result);
+        return result;
+    }
+private:
+    void backtrack(const string& s, int start , vector<string> &path , vector<vector<string>>& result){
+        if(start == s.length()){
+            result.push_back(path);
+            return;
+        }
+
+        for(int end = start+1 ; end<=s.length() ; ++end){
+            if(isPallindrome(s, start , end-1)){
+                path.push_back(s.substr(start , end-start));
+                backtrack(s,end,path, result);
+                path.pop_back();
+            }
+        }
+    }
+    bool isPallindrome(const string& s , int left , int right){
+        while(left < right){
+                if(s[left++] != s[right--]){
+                    return false;
+                }
+            }
+
+        
+        return true;
+    }
+};
