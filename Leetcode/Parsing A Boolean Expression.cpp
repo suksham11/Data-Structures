e08948cc358e043dbc92ec96755b135664fa54e3
suksham <129354626+suksham11@users.noreply.github.com>
2024-10-20 23:43:17 +0530

    Create Parsing A Boolean Expression.cpp

---
+++ Leetcode/Parsing A Boolean Expression.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    char solveoperator(vector<char>& values, char op){
+      if(op == '!'){
+        return values[0] == 't' ? 'f' : 't';
+      }
+      if(op == '&'){
+        return any_of(begin(values) , end(values), [](char ch) {return ch == 'f';}) ? 'f' : 't';
+      }
+      if(op == '|'){
+        return any_of(begin(values) , end(values), [](char ch) {return ch == 't';}) ? 't' : 'f';
+      }
+      return 't';
+    }
+
+    bool parseBoolExpr(string expression) {
+        int n = expression.size();
+        stack<char> st;
+        for(int i=0 ; i<n ; i++){
+            if(expression[i] == ','){
+                continue;
+            }
+            if(expression[i] == ')'){
+                vector<char> values;
+            
+
+            while(st.top() != '('){
+                values.push_back(st.top());
+                st.pop();
+            }
+
+            st.pop();
+            char op = st.top();
+            st.pop();
+
+            st.push(solveoperator(values, op));
+        }else{
+            st.push(expression[i]);
+        }
+    }
+        return st.top() == 't';
+    }
+    
+};
