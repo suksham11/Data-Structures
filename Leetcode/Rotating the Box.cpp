210fd329c9ed6ec976d2b2e2187477a2434b53da
suksham <129354626+suksham11@users.noreply.github.com>
2024-11-23 22:17:47 +0530

    Create  Rotating the Box.cpp

---
+++ Leetcode/Rotating the Box.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+       int m = box.size();
+       int n = box[0].size();
+       vector<vector<char>> result(n, vector<char>(m));
+
+       for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+            result[i][j] = box[j][i];
+        }
+       } 
+
+       for(int i=0 ; i<n ; i++){
+        reverse(result[i].begin() , result[i].end());
+       }
+
+       for(int j=0 ; j<m ; j++){
+        for(int i=n-1 ; i>=0 ; i--){
+            if(result[i][j] == '.'){
+                int nextrowwithstone = -1;
+
+                for(int k=i-1 ; k>=0 ; k--){
+                    if(result[k][j] == '*') break;
+                    if(result[k][j] == '#'){
+                        nextrowwithstone = k;
+                        break;
+                    }
+                }
+                if(nextrowwithstone != -1){
+                    result[nextrowwithstone][j] = '.';
+                    result[i][j] = '#';
+                }
+            }
+        }
+       }
+       return result;
+    }
+};
