787586444ab8a5f7611fd9446e4e157f82d8343a
Suksham <129354626+suksham11@users.noreply.github.com>
2025-02-05 04:15:07 +0530

    Create  Maximum Ascending Subarray Sum.cpp

---
+++ Leetcode/Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+       int n = nums.size();
+       int maxsum = 0;
+       int sum = nums[0];
+       for(int i=1 ; i<n ; i++){
+        if(nums[i] > nums[i-1]){
+            sum += nums[i];
+        }
+        else{
+            maxsum = max(maxsum , sum);
+            sum = nums[i];
+        }
+       }
+    return max(maxsum, sum);
+    }
+};
