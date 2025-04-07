9d7e8adb0bc5be4533c79d5f193c91b9a2816634
Suksham <129354626+suksham11@users.noreply.github.com>
2025-04-07 22:12:33 +0530

    Create Partition Equal Subset Sum.cpp

---
+++ Leetcode/Partition Equal Subset Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int t[201][20001];
+    bool solve(vector<int>& nums , int i , int x){
+        if(x==0){
+            return true;
+        }
+        if(i >= nums.size()) return false;
+        
+        if(t[i][x] != -1){ return t[i][x]; }
+
+        bool take = false;
+        if(nums[i] <= x){
+            take = solve(nums, i+1 , x-nums[i]);
+        }
+        bool nottake = solve(nums, i+1 , x);
+        return t[i][x] = take || nottake;
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        int S = accumulate(begin(nums) , end(nums), 0);
+
+        if(S % 2 != 0){
+            return false;
+        }
+        memset(t, -1 , sizeof(t));
+        vector<vector<int>> t(n+1);
+        int x = S/2;
+        return solve(nums, 0 , x);
+    }
+};
