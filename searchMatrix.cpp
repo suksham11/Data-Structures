f7b0297ab575c4d0d9cfcb08814d0b5d8f60596f
Suksham <129354626+suksham11@users.noreply.github.com>
2025-05-13 22:25:20 +0530

    Create searchMatrix.cpp

---
+++ searchMatrix.cpp
@@ -0,0 +1,21 @@
+ 
+   if(matrix.empty() || matrix[0].empty()){
+       return false;
+   }
+
+   int rows = matrix.size();
+   int cols = matrix[0].size();
+   int row = 0;
+   int col = cols - 1;
+   while(row < rows && col >= 0 ){
+       if(matrix[row][col] == target){
+            return true;
+        }
+       else if(matrix[row][col] < target){
+                row++;
+        }
+       else{
+           col--;
+        }
+    }
+    return false;
